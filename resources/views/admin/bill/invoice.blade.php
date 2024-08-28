2446141eae4d2d2ece772405b554b42a87378a11
shafireza <19103145@iubat.edu>
2024-08-29 00:05:32 +0600

    design invoice

--- resources/views/admin/bill/invoice.blade.php
+++ resources/views/admin/bill/invoice.blade.php
@@ -7,7 +7,6 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-
                     </div>
                 </div>
             </div><!-- /.container-fluid -->
@@ -17,26 +16,22 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-
-
-
                         <!-- Main content -->
                         <div class="invoice p-3 mb-3">
+
                             <!-- title row -->
                             <div class="row">
                                 <h4 class="mx-auto">Invoice</h4>
                                 <div class="col-12">
                                     <h4>
-                                        <small
-                                            class="float-right">Date:{{ \Carbon\Carbon::parse($bill->bill_date)->format('d-m-Y') }}</small>
+                                        <small class="float-right">Date:
+                                            {{ \Carbon\Carbon::parse($bill->bill_date)->format('d-m-Y') }}</small>
                                     </h4>
                                 </div>
-                                <!-- /.col -->
                             </div>
                             <!-- info row -->
                             <div class="row invoice-info">
                                 <div class="col-sm-4 invoice-col">
-
                                     <address>
                                         <strong>Power Zone</strong><br>
                                         Bosila Housing,<br>
@@ -44,21 +39,13 @@
                                         Phone: 01722-533538, 01918-750912<br>
                                     </address>
                                 </div>
-                                <!-- /.col -->
                                 <div class="col-sm-4 invoice-col">
-
                                     <address>
                                         <strong>{{ $customer->name }}</strong><br>
                                         {{ $customer->address }}<br>
-
-
                                     </address>
                                 </div>
-                                <!-- /.col -->
-
-                                <!-- /.col -->
                             </div>
-                            <!-- /.row -->
 
                             <!-- Table row -->
                             <div class="row">
@@ -80,9 +67,7 @@
                                                 <th>Quantity</th>
                                                 <th>Unit Price</th>
                                                 <th>Discount</th>
-
                                                 <th>Total Amount</th>
-
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -103,20 +88,15 @@
                                                             {{ number_format($product->discount) }}
                                                         @endif
                                                     </td>
-
                                                     <td>{{ $product->total_amount }}</td>
-
                                                 </tr>
                                             @endforeach
                                         </tbody>
                                     </table>
                                 </div>
-                                <!-- /.col -->
                             </div>
-                            <!-- /.row -->
 
                             <div class="row">
-                                <!-- accepted payments column -->
                                 @if ($previousBills->count() > 0)
                                     <div class="col-6">
                                         <table class="table table-striped">
@@ -124,7 +104,6 @@
                                                 <tr>
                                                     <th>Bill ID</th>
                                                     <th>Bill Date</th>
-                                                    {{-- <th>Status</th> --}}
                                                     <th>Paid Amount</th>
                                                     <th>Due Amount</th>
                                                 </tr>
@@ -136,16 +115,12 @@
                                                         <td>{{ $previousBill->created_at }}</td>
                                                         <td>{{ $previousBill->receivable_amount }}</td>
                                                         <td>{{ $previousBill->due_amount }}</td>
-
-
                                                     </tr>
                                                 @endforeach
                                             </tbody>
                                         </table>
                                     </div>
                                 @endif
-
-                                <!-- /.col -->
                                 <div class="col-6 ml-auto">
                                     <div class="table-responsive">
                                         <table class="table">
@@ -178,12 +153,8 @@
                                         </table>
                                     </div>
                                 </div>
-
-                                <!-- /.col -->
                             </div>
-                            <!-- /.row -->
 
-                            <!-- this row will not appear when printing -->
                             <div class="row no-print">
                                 <div class="col-12">
                                     <a href="#" onclick="printPage()" class="btn btn-default"><i
@@ -200,11 +171,46 @@
                             </script>
 
                         </div>
-                        <!-- /.invoice -->
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+                    </div>
+                </div>
+            </div>
         </section>
-        <!-- /.content -->
     </div>
 @endsection
+
+<style>
+    .invoice {
+    position: relative;
+    padding: 2rem;
+    background: linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url("{{ asset('admin/images/Capture.jpg') }}") no-repeat center center / cover;
+    border: none;
+    height: 100vh;
+    box-shadow: none;
+    overflow: hidden;
+}
+
+    .invoice::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url("{{ asset('admin/images/Capture.jpg') }}");
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
+        opacity: 1; /* Adjust the opacity as needed */
+        z-index: -1; /* Ensure the background is behind the content */
+    }
+
+    @media print {
+        .invoice::before {
+            opacity: 0.5; /* Adjust the opacity for print if needed */
+        }
+
+        .no-print {
+            display: none; /* Hide print buttons during printing */
+        }
+    }
+</style>
